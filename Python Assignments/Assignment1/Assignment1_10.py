93587df2d778df892be08feddb5249445a195890
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2022-01-03 18:38:21 +0530

    Add files via upload

---
+++ Python Assignments/Assignment1/Assignment1_10.py
@@ -0,0 +1,17 @@
+#Write a program which accepts name from user and display length of its name.
+
+
+
+
+def main():
+    print("Enter Name :");
+    Name = input();
+    
+    #for Nm in range(len(Name)):
+    print(len(Name));
+
+
+if __name__ == "__main__":
+    main();
+    
+    
