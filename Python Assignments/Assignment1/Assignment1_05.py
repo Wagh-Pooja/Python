dc78017c178435c0a14d655723c015e57199a0fa
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2022-01-03 18:35:13 +0530

    Add files via upload

---
+++ Python Assignments/Assignment1/Assignment1_05.py
@@ -0,0 +1,15 @@
+#Write a program which display 10 to 1 on screen.
+
+
+def Display():
+    for Num in range(10,0,-1):
+        print(Num);
+
+
+def main():
+    Display();
+
+
+if __name__ == "__main__":
+    main();
+    
