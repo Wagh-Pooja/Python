77e7c1c6157cd746e2fa7411429fc9efc8352ae0
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2022-01-04 15:27:56 +0530

    Add files via upload

---
+++ Python Assignments/Assignment2/Assignment2_08.py
@@ -0,0 +1,21 @@
+#Write a program which accept one number and display below pattern.
+
+
+def Pattern(Num):
+    for i in range(1,Num+1):
+        
+        for j in range(1,Num+1):
+            if(i>=j):
+                print(j,end=' ');
+        print();
+
+
+
+def main():
+    print("Enter Number :");
+    No = int(input());
+    
+    Pattern(No);
+
+if __name__ == "__main__":
+    main();
