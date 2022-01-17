43fd3004716e65a3cfd0345ba377da69d49869fb
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2022-01-17 19:55:43 +0530

    Add files via upload

---
+++ Python Programs/Datatypes/04_Datatypes.py
@@ -0,0 +1,44 @@
+#Numeric Datatypes
+
+No = 11;
+print(type(No));        #int
+print(No);              #11
+
+value = 3.14;
+print(type(value));     #float
+print(value);           #3.14
+
+X = True;
+print(type(X));         #bool
+print(X);               #True
+
+data = 10+5j;           #X+Yj   x is Real part and Y is Imaginary part      
+print(type(data));      #complex
+print(data);            #10+5j
+
+
+###################################################################################
+
+
+i = None;
+print(type(i));         #NoneType
+print(i);               #None
+
+
+###################################################################################
+
+
+value = 11;
+print(type(value));     #int
+print(value);           #11
+
+value = None;
+print(type(value));     #NoneType
+print(value);           #None
+
+value = 3.14;
+print(type(value));     #float
+print(value);           #3.14
+
+demo = None;
+print(demo);
