69405022a164cfe02cbd859a15590458422a529e
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2022-01-17 19:54:55 +0530

    Add files via upload

---
+++ Python Programs/Datatypes/02_Datatypes.py
@@ -0,0 +1,17 @@
+No = 15;
+
+Name = "Pooja";
+
+Marks = 80.81;
+
+print(No);             #15
+
+print(Name);           #Pooja
+
+print(Marks);          #80.81
+
+print(type(No));       #<class 'int'>
+
+print(type(Name));     #<class 'str'>
+
+print(type(Marks));    #<class 'float'>
