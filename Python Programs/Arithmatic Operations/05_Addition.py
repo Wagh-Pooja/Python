20d52ba30a89eb079853aacb5a758f3be6fa1997
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2022-01-19 14:01:32 +0530

    Add files via upload

---
+++ Python Programs/Arithmatic Operations/05_Addition.py
@@ -0,0 +1,7 @@
+No1 = int(input("Enter First Number : "));      #20
+
+No2 = int(input("Enter Second Number : "));     #30
+
+Ans = No1 + No2;
+
+print("Addition is :",Ans);                       #50
