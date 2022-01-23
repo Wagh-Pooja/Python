423f80e60e311f52f087c2825253f5efce2ea649
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2022-01-23 20:58:04 +0530

    Add files via upload

---
+++ Python Programs/Array/01_Array.py
@@ -0,0 +1,11 @@
+#import array
+
+def main():
+    arr = [10,20.5,"Marvellous",False];
+    print(arr);
+    print("Length of list is :",len(arr));
+    print("Type of arr is :",type(arr[1]));
+
+
+if __name__ == "__main__":
+    main();
