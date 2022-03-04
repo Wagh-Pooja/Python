9118a8ce3708571152d8db2bd7c11a8c481204f4
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2022-03-04 21:34:10 +0530

    Add files via upload

---
+++ Python Programs/Recursion/05_Recursion.py
@@ -0,0 +1,18 @@
+i = 0  
+
+
+def fun():
+    global i
+    
+    if(i<5):
+        print(i)
+        i = i + 1   #i++
+        fun()       #recursive call
+        
+
+def main():
+    fun()
+
+
+if __name__ == "__main__":
+    main();
