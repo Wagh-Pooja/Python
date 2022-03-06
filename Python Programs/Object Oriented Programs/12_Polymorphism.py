216b9f4d201a9647a49bb638e49b33ea55dab601
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2022-03-06 21:47:38 +0530

    Add files via upload

---
+++ Python Programs/Object Oriented Programs/12_Polymorphism.py
@@ -0,0 +1,20 @@
+class Demo:
+    def __init__(self):
+        self.i = 10
+        self.j = 20
+
+    def Add(self,a):
+        print("Inside Add with one parameter")
+
+    def Add(self,a,b):
+        print("Inside Add with two parameters")
+
+def main():
+    obj = Demo()
+    #obj.Add(11)
+    obj.Add(11,21)
+
+if __name__ == "__main__":
+    main()
+
+
