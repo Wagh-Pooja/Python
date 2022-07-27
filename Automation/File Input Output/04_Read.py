92dfb692d879abba04280e44baea2d1a0bffd1bf
Pooja Maruti Wagh <91519272+Wagh-Pooja@users.noreply.github.com>
2022-07-27 22:50:39 +0530

    File I/O

---
+++ Automation/File Input Output/04_Read.py
@@ -0,0 +1,15 @@
+def main():
+    print("Enter the file name that you want to Open : ")
+    name = input()
+
+    fd = open(name, "r")
+
+    data = fd.read(5)
+
+    print("Data from file is : ", data)
+
+    fd.close()
+
+
+if __name__ == "__main__":
+    main()
