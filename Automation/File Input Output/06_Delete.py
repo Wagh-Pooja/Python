540c0f9b6dcd84b55a15cb961107e5846f163304
Pooja Maruti Wagh <91519272+Wagh-Pooja@users.noreply.github.com>
2022-07-28 22:04:40 +0530

    File I/O

---
+++ Automation/File Input Output/06_Delete.py
@@ -0,0 +1,17 @@
+import os
+
+def main():
+    print("Enter the file name that you want to Delete : ")
+    name = input()
+
+    if os.path.exists(name):
+        os.remove(name)
+
+        print("File gets deleted")
+
+    else:
+        print("There is no such file")
+
+
+if __name__ == "__main__":
+    main()
